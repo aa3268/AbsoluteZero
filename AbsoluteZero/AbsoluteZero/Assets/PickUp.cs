527deaa214cc0ac5a44bb7414765a60d3149128e
aa3268 <aa3268@github.com>
2016-01-18 00:54:09 -0500

    Initial

---
+++ AbsoluteZero/AbsoluteZero/Assets/PickUp.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PickUp : MonoBehaviour {
+
+	public Slider personal;
+
+	public float amount;
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.name.Equals ("Player")) {
+			if (personal.value <= personal.maxValue - amount) {
+				personal.value += amount;
+			} else {
+				personal.value += personal.maxValue - personal.value;
+
+			}
+			Destroy (gameObject);
+		}
+	}
+}
